aa98c3c54946cefe8a1478b53d1f405a27d244ac
natarajank <natarajank29@gmail.com>
2019-11-05 15:05:21 -0600

    added seq

--- rest-web-service/src/main/resources/data.sql
+++ rest-web-service/src/main/resources/data.sql
@@ -1,4 +1,5 @@
-insert into product values (111,999.99,'TV','Samson');
-insert into product values (222,650.45,'Washer','Samson');
-insert into product values (333,550,'Dryer','Samson');
-insert into product values (444,600,'AC','Samson');
+insert into product values (1,999.99,'TV','Samson');
+insert into product values (2,650.45,'Washer','Samson');
+insert into product values (3,550,'Dryer','Samson');
+insert into product values (4,600,'AC','Samson');
+ALTER SEQUENCE HIBERNATE_SEQUENCE RESTART WITH 5;
